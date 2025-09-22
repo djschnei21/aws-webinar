1a08e82103b863d1ffe11eee13db1cc6c438c8dd
Dan Schneider <32646250+djschnei21@users.noreply.github.com>
2025-09-22 15:35:15 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform { 
+  cloud { 
+    
+    organization = "djs-tfcb" 
+
+    workspaces { 
+      name = "vault-backed-aws-credentials" 
+    } 
+  }
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+    }
+    random = {
+      source  = "hashicorp/random"
+    }
+  } 
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "vault" {}
+
+resource "vault_kv_secret_v2" "example" {
+  mount                      = "kv"
+  name                       = "super-secret-data"
+  data_json                  = jsonencode(
+  {
+    zip       = "zap",
+    foo       = "bar"
+  }
+  )
+}
+
+resource "random_pet" "example" {}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_pet.example.id
+}
---
+++ terraform-dynamic-creds-policy.hcl
@@ -0,0 +1,23 @@
+# Allow tokens to query themselves
+path "auth/token/lookup-self" {
+  capabilities = ["read"]
+}
+
+# Allow tokens to renew themselves
+path "auth/token/renew-self" {
+    capabilities = ["update"]
+}
+
+# Allow tokens to revoke themselves
+path "auth/token/revoke-self" {
+    capabilities = ["update"]
+}
+
+# Configure the actual secrets the token should have access to
+path "kv/*" {
+  capabilities = ["create", "read", "update", "patch", "delete", "list"]
+}
+
+path "aws/*" {
+ capabilities = ["read"]
+}
